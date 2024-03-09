f7bb27c7f34621693a5310bc6186404995e96fb3
Kamx <kamxsyt@gmail.com>
2024-03-09 18:40:29 +0100

    mapa dalej

--- Assets/Skrypty/PlayerLight.cs
+++ Assets/Skrypty/PlayerLight.cs
@@ -6,25 +6,67 @@ public class PlayerLight : MonoBehaviour
 {
     int player;
     public List<Transform> lightsPlayerIsIn;
+    public List<Transform> potentialLights;
+    public List<Transform> toChange;
     void Start()
     {
         player = GetComponent<Movement>().Player;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        foreach(Transform light in potentialLights)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(light.position, transform.position - light.position, 100.0f) ;
+            Debug.DrawRay(light.position, transform.position - light.position);
+            //if(hit.collider.tag !="Player" && playerInside)
+            if (hit.collider.tag == "Player")
+            {
+                toChange.Add(light);
+                //lightsPlayerIsIn.Add(light);
+                //potentialLights.Remove(light);
+            }
+            /*
+            else if (hit.collider.tag == "Player")
+            {
+                lightsPlayerIsIn.Add(light);
+                collision.GetComponent<PlayerLight>().lightsPlayerIsIn.Add(transform);
+            }
+            */
+        }
+        foreach(Transform light in lightsPlayerIsIn)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(light.position, transform.position - light.position, 100.0f) ;
+            Debug.DrawRay(light.position, transform.position - light.position);
+            if (hit.collider.tag != "Player")
+            {
+                Debug.Log("Wyjebaæ œwiat³o");
+                toChange.Add(light);
+                // lightsPlayerIsIn.Remove(light);
+                //potentialLights.Add(light);
+            }
+
+        }
+        foreach(Transform light in toChange)
+        {
+            if(lightsPlayerIsIn.Contains(light))
+            {
+                lightsPlayerIsIn.Remove(light);
+                potentialLights.Add(light);
+            }else
+            {
+                lightsPlayerIsIn.Add(light);
+                potentialLights.Remove(light);
+            }
+        }
+        toChange.Clear();
         if(player==1&& lightsPlayerIsIn.Count == 0)
         {
             Die();
         }else if(player ==2 && lightsPlayerIsIn.Count>0)
         {
             Die();
-        } 
-    }
-
-    private void FixedUpdate()
-    {
+        }
     }
 
     void Die()
