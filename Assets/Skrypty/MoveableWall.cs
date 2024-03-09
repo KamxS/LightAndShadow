cd3b26d95fdf575852778c150b2cfe6355371359
Kamx <kamxsyt@gmail.com>
2024-03-09 18:03:30 +0100

    dalejmapa

--- Assets/Skrypty/MoveableWall.cs
+++ Assets/Skrypty/MoveableWall.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MoveableWall : MonoBehaviour
 {
-
+    [SerializeField] Transform towards;
+    [SerializeField] float moveSpeed;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
     public void Move()
     {
-
+        //transform.position = Vector2.MoveTowards(transform.position, towards.position, 1.0f);
+        Debug.Log(towards.position - transform.position);
+        transform.DOMove(towards.position, moveSpeed);
     }
 }
