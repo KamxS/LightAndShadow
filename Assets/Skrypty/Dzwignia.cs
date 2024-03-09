f7d9bad90ba5139313f9edb9b1b3363a073b5940
Kamx <kamxsyt@gmail.com>
2024-03-09 16:52:21 +0100

    Dzwignia+-

---
+++ Assets/Skrypty/Dzwignia.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Dzwignia : MonoBehaviour
+{
+    bool playerIn;
+    [SerializeField] bool isOn;
+    public UnityEvent on;
+    public UnityEvent off;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && playerIn)
+        {
+            if(on)
+            {
+                off.Invoke();
+                isOn = false;
+            }else
+            {
+                on.Invoke();
+                isOn = true;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            playerIn = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            playerIn = false;
+        }   
+    }
+}
